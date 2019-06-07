d432a18ead027cfd4a922caa9cef2188c819d929
Quinn Turner <quinnturnertech@gmail.com>
2019-06-07 12:08:41 -0400

    feat(web-api): Change default port to 3001

--- packages/web-api/src/index.ts
+++ packages/web-api/src/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get('/api/hello', (req, res) => {
   res.send('Hello, World!');
