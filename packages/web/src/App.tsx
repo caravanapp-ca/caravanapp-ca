598abd4a9d053592eb11da99d44c76775dd3a26e
Matt Cherry <mrcherry@uwaterloo.ca>
2019-06-12 17:36:00 -0400

    Initial club setup

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './views/home/Home';
+import Club from './views/club/Club';
 import Footer from './components/Footer';
 
 export interface AppProps {}
@@ -12,6 +13,7 @@ export function App(props: AppProps) {
       <div>
         <Header />
         <Route exact path="/" component={Home} />
+        <Route path="/club" component={Club} />
         <Footer />
       </div>
     </Router>
