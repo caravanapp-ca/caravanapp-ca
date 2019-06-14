b9ec3fcf3d2edaa4fe1781d7603ca2841ebdcf6e
Matt Cherry <mrcherry@uwaterloo.ca>
2019-06-13 20:45:57 -0400

    Basic setup for the clubs page

--- packages/web/src/views/club/Club.tsx
+++ packages/web/src/views/club/Club.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import './ClubHeader.css';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+
+}));
 
 export default function Club() {
+  const classes = useStyles();
+
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div>
+      <div className="bg-image">
+        <div className="bg-shade">
+        </div>
+      </div>
+      <div className="header-text-container">
+        <Typography>Currently Reading</Typography>
+        <Typography>The Name of the Wind</Typography>
+        <Typography>Patrick Rothfuss, 2007</Typography>
+        <Typography>Fantasy</Typography>
+      </div>
+    </div>
   )
 }
