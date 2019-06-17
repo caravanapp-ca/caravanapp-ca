7f67c5f2c9a04a79440808ed72ffc255a209116f
Yusuf <yhbk12@gmail.com>
2019-06-17 16:27:29 -0400

    Update Home.tsx

--- packages/web/src/views/home/Home.tsx
+++ packages/web/src/views/home/Home.tsx
@@ -106,6 +106,7 @@ export default function Home(props: HomeProps) {
               align="center"
               color="textPrimary"
               gutterBottom
+              style={{ marginTop: 20 }}
             >
               Album layout
             </Typography>
