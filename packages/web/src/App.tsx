9c5dbdffe14054913d3a98c0a27032a840913ef7
Matt Cherry <mrcherry@uwaterloo.ca>
2019-06-17 14:42:43 -0400

    Fixed issues with club/create rendering club info

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './views/home/Home';
@@ -18,7 +23,7 @@ export function App(props: AppProps) {
   const user = useInitializeUser();
   return (
     <Router>
-      <div>
+      <Switch>
         <Route exact path="/" component={HomeRedirect} />
         <Route
           exact
@@ -26,17 +31,17 @@ export function App(props: AppProps) {
           render={props => <Home {...props} user={user} />}
         />
         <Route
-          path="/club/:id"
-          render={props => <Club {...props} user={user} />}
-        />
-        <Route
           exact
           path="/club/create"
           render={props => <CreateClub {...props} user={user} />}
         />
+        <Route
+          path="/club/:id"
+          render={props => <Club {...props} user={user} />}
+        />
         <Route exact path="/findbooks" component={FindBooks} />
-        <Footer />
-      </div>
+      </Switch>
+      <Footer />
     </Router>
   );
 }
