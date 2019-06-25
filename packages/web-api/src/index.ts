9ff8ed8103b7b6b2464c9f287fbef258b0bfe89b
Quinn Turner <quinnturnertech@gmail.com>
2019-06-25 11:03:02 -0400

    Undo redirect

--- packages/web-api/src/index.ts
+++ packages/web-api/src/index.ts
@@ -32,17 +32,6 @@ import { ReadingDiscordBot } from './services/discord';
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  if (process.env.NODE_ENV === 'production') {
-    app.use((req, res, next) => {
-      if (req.secure) {
-        // request was via https, so do no special handling
-        next();
-      } else {
-        // request was via http, so redirect to https
-        res.redirect('https://' + req.headers.host + req.url);
-      }
-    });
-  }
 
   // logs in
   const discordClient = ReadingDiscordBot.getInstance();
