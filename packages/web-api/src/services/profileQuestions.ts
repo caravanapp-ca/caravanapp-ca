d43569fd9f7f0a7779a905c9be7d49a3ce9ee574
Yusuf <yhbk12@gmail.com>
2019-06-28 11:08:49 -0400

    Update profileQuestions.ts

--- packages/web-api/src/services/profileQuestions.ts
+++ packages/web-api/src/services/profileQuestions.ts
@@ -1,8 +1,8 @@
 import ProfileQuestionsModel from '../models/profileQuestions';
 
 export const getProfileQuestions = async () => {
-  const profileQuestionsDoc = await ProfileQuestionsModel.findOne({
-    createdAt: -1,
-  }).exec();
+  const profileQuestionsDoc = await ProfileQuestionsModel.findOne()
+    .sort({ created_at: -1 })
+    .exec();
   return profileQuestionsDoc;
 };
