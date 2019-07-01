58430e4f9f988a65d2425d3eadbe8449061639b5
Yusuf <yhbk12@gmail.com>
2019-07-01 17:13:23 -0400

    Update ProfileQuestionsCarousel.tsx

--- packages/web/src/components/ProfileQuestionsCarousel.tsx
+++ packages/web/src/components/ProfileQuestionsCarousel.tsx
@@ -7,8 +7,9 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Paper from '@material-ui/core/Paper';
 import { Services } from '@caravan/buddy-reading-types';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -18,18 +19,42 @@ const useStyles = makeStyles((theme: Theme) =>
       flexWrap: 'wrap',
       justifyContent: 'space-around',
       overflow: 'hidden',
-      padding: theme.spacing(10, 0, 10),
+      padding: theme.spacing(10, 0, 0),
+      color: theme.palette.text.secondary,
     },
     gridList: {
       flexWrap: 'nowrap',
       transform: 'translateZ(0)',
+      width: '90%',
     },
     title: {
       color: theme.palette.primary.light,
     },
     titleBar: {
-      background:
-        'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+      // background:
+      //   'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+    },
+    paper: {
+      height: '80%',
+      position: 'relative',
+      color: theme.palette.text.secondary,
+      borderRadius: '25px',
+      border: '2px solid #D3D3D3',
+    },
+    textDiv: {
+      padding: theme.spacing(15, 2, 5),
+    },
+    questionText: {
+      fontWeight: 600,
+      paddingBottom: theme.spacing(3),
+    },
+    defaultAnswerText: {
+      fontStyle: 'italic',
+    },
+    buttonDiv: {
+      position: 'absolute',
+      bottom: 30,
+      left: 30,
     },
   })
 );
@@ -49,16 +74,37 @@ export default function ProfileQuestionsCarousel(
     <div className={classes.root}>
       <GridList className={classes.gridList} cols={2.5}>
         {questions.map(question => (
-          <GridListTile key={question.id}>
-            <img src="https://cdn1us.denofgeek.com/sites/denofgeekus/files/harry_potter_footage_rpg_leak.jpeg" />
-            <GridListTileBar
-              title={question.title}
-              classes={{
-                root: classes.titleBar,
-                title: classes.title,
-              }}
-              actionIcon={<Button>ANSWER</Button>}
-            />
+          <GridListTile
+            key={question.id}
+            cols={1}
+            rows={4}
+            style={{ backgroundColor: 'white' }}
+          >
+            <Paper className={classes.paper}>
+              <div className={classes.textDiv}>
+                <Typography
+                  variant="h5"
+                  component="h2"
+                  color="textPrimary"
+                  className={classes.questionText}
+                >
+                  {question.title}...
+                </Typography>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  color="textSecondary"
+                  className={classes.defaultAnswerText}
+                >
+                  {question.subtitle}
+                </Typography>
+              </div>
+              <div className={classes.buttonDiv}>
+                <Button variant="contained" color="primary">
+                  ANSWER
+                </Button>
+              </div>
+            </Paper>
           </GridListTile>
         ))}
       </GridList>
