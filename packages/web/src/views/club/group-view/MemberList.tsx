72bfc08a9fed3a62cbc089e5edf579060de87927
Matt Cherry <mrcherry@uwaterloo.ca>
2019-07-09 21:15:00 -0400

    Fixed club page layout for large groups

--- packages/web/src/views/club/group-view/MemberList.tsx
+++ packages/web/src/views/club/group-view/MemberList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import { List, Avatar, IconButton } from '@material-ui/core';
+import { List, Avatar, IconButton, Grid } from '@material-ui/core';
 import StarIcon from '@material-ui/icons/Star';
 import ListElementAvatar from '../../../components/ListElementAvatar';
 import FreeGroupSlotListElement from '../../../components/FreeGroupSlotListElement';
@@ -20,36 +20,74 @@ export default function MemberList(props: MemberListProps) {
 
   let emptySlots = [];
   for (let i = 0; i < freeSlots; i++) {
-    emptySlots.push(<FreeGroupSlotListElement key={i} />);
+    emptySlots.push(
+      <Grid item xs={12} sm={6}>
+        <FreeGroupSlotListElement key={i} />
+      </Grid>
+    );
   }
 
   return (
-    <List dense={false}>
+    <Grid
+      container
+      direction="row"
+      justify="flex-start"
+      alignItems="flex-start"
+    >
       {members.map(m => (
-        <ListElementAvatar
-          button={m.urlSlug ? true : undefined}
-          link={m.urlSlug ? `/user/${m.urlSlug}` : undefined}
-          key={m._id}
-          avatarElement={
-            m.photoUrl ? (
-              <Avatar alt={m.name || m.discordUsername} src={m.photoUrl} />
-            ) : (
-              undefined
-            )
-          }
-          primaryText={m.name || m.discordUsername}
-          secondaryElement={
-            props.ownerId === m._id ? (
-              <IconButton edge="end" aria-label="Star" disabled={true}>
-                <StarIcon />
-              </IconButton>
-            ) : (
-              undefined
-            )
-          }
-        />
+        <Grid item xs={12} sm={6}>
+          <ListElementAvatar
+            button={m.urlSlug ? true : undefined}
+            link={m.urlSlug ? `/user/${m.urlSlug}` : undefined}
+            key={m._id}
+            avatarElement={
+              m.photoUrl ? (
+                <Avatar alt={m.name || m.discordUsername} src={m.photoUrl} />
+              ) : (
+                undefined
+              )
+            }
+            primaryText={m.name || m.discordUsername}
+            secondaryElement={
+              props.ownerId === m._id ? (
+                <IconButton edge="end" aria-label="Star" disabled={true}>
+                  <StarIcon />
+                </IconButton>
+              ) : (
+                undefined
+              )
+            }
+          />
+        </Grid>
       ))}
       {emptySlots.length > 0 && emptySlots}
-    </List>
+    </Grid>
+    // <List dense={false}>
+    //   {members.map(m => (
+    //     <ListElementAvatar
+    //       button={m.urlSlug ? true : undefined}
+    //       link={m.urlSlug ? `/user/${m.urlSlug}` : undefined}
+    //       key={m._id}
+    //       avatarElement={
+    //         m.photoUrl ? (
+    //           <Avatar alt={m.name || m.discordUsername} src={m.photoUrl} />
+    //         ) : (
+    //           undefined
+    //         )
+    //       }
+    //       primaryText={m.name || m.discordUsername}
+    //       secondaryElement={
+    //         props.ownerId === m._id ? (
+    //           <IconButton edge="end" aria-label="Star" disabled={true}>
+    //             <StarIcon />
+    //           </IconButton>
+    //         ) : (
+    //           undefined
+    //         )
+    //       }
+    //     />
+    //   ))}
+    //   {emptySlots.length > 0 && emptySlots}
+    // </List>
   );
 }
