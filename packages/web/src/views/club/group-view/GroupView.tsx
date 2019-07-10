997fb0bb062fa45a7d2ae32f85f06c1216c093c0
Matt Cherry <mrcherry@uwaterloo.ca>
2019-07-10 18:03:38 -0400

    Better labels for GroupView

--- packages/web/src/views/club/group-view/GroupView.tsx
+++ packages/web/src/views/club/group-view/GroupView.tsx
@@ -114,7 +114,7 @@ export default function GroupView(props: GroupViewProps) {
       <div>
         <div className={classes.sectionContainer}>
           <Typography variant={'h6'} className={classes.sectionLabel}>
-            About the Group
+            Bio
           </Typography>
           <div className={classes.editableTextFieldContainer}>
             <TextField
@@ -231,7 +231,7 @@ export default function GroupView(props: GroupViewProps) {
       <div>
         <div className={classes.sectionContainer}>
           <Typography variant={'h6'} className={classes.sectionLabel}>
-            About the Group
+            Bio
           </Typography>
           {bio && <Typography>{bio}</Typography>}
         </div>
