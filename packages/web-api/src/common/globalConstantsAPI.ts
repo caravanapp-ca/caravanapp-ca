bcfd96198fee9d48f7cc7d8c5c9464663d2b4415
Matt Cherry <mrcherry@uwaterloo.ca>
2019-07-30 17:20:37 -0400

    Added production #general-chat ID

--- packages/web-api/src/common/globalConstantsAPI.ts
+++ packages/web-api/src/common/globalConstantsAPI.ts
@@ -1,3 +1,5 @@
-// Line 1 is test, line 2 is production.
 // TODO: switch test with production
+// buddy-reading-test
 export const discordGenChatChId = '589194387968491532';
+// Production
+// export const discordGenChatChId = '592761082523680806';
