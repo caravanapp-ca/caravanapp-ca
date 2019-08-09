66b865cac299882e138a53104e403fc0582923a1
Yusuf <yhbk12@gmail.com>
2019-08-09 16:16:00 -0400

    Prettier fix

--- packages/web-api/src/routes/userRoutes.ts
+++ packages/web-api/src/routes/userRoutes.ts
@@ -100,19 +100,19 @@ router.get('/', async (req, res) => {
       return user;
     })
     .filter(c => c !== null);
-    if ((search && search.length) > 0) {
-      const fuseOptions: Fuse.FuseOptions<Services.GetUsers['users']> = {
-        // TODO: Typescript doesn't like the use of keys here.
-        // @ts-ignore
-        keys: [
-          { name: 'name', weight: 4 / 10 },
-          { name: 'shelf.notStarted.title', weight: 3 / 10 },
-          { name: 'shelf.notStarted.author', weight: 3 / 10 },
-        ],
-      };
-      const fuse = new Fuse(filteredUsers, fuseOptions);
-      filteredUsers = fuse.search(search);
-    }
+  if ((search && search.length) > 0) {
+    const fuseOptions: Fuse.FuseOptions<Services.GetUsers['users']> = {
+      // TODO: Typescript doesn't like the use of keys here.
+      // @ts-ignore
+      keys: [
+        { name: 'name', weight: 4 / 10 },
+        { name: 'shelf.notStarted.title', weight: 3 / 10 },
+        { name: 'shelf.notStarted.author', weight: 3 / 10 },
+      ],
+    };
+    const fuse = new Fuse(filteredUsers, fuseOptions);
+    filteredUsers = fuse.search(search);
+  }
   const result: Services.GetUsers = {
     users: filteredUsers,
   };
