eb279f7dfdc8a8e79325edcce573651ee7fdd26e
Yusuf <yhbk12@gmail.com>
2019-08-09 17:55:21 -0400

    Prettier

--- packages/web-api/src/routes/userRoutes.ts
+++ packages/web-api/src/routes/userRoutes.ts
@@ -104,9 +104,7 @@ router.get('/', async (req, res) => {
     const fuseOptions: Fuse.FuseOptions<Services.GetUsers['users']> = {
       // TODO: Typescript doesn't like the use of keys here.
       // @ts-ignore
-      keys: [
-        { name: 'shelf.notStarted.title', weight: 1 / 1 },
-      ],
+      keys: [{ name: 'shelf.notStarted.title', weight: 1 / 1 }],
     };
     const fuse = new Fuse(filteredUsers, fuseOptions);
     filteredUsers = fuse.search(search);
