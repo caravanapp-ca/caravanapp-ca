ea8f99a143aeffb6ce83e9a97d1b429a3e240520
Matt Cherry <mrcherry@uwaterloo.ca>
2019-08-20 15:19:24 -0400

    Trying new session retrieval

--- packages/web-api/src/services/session.ts
+++ packages/web-api/src/services/session.ts
@@ -1,5 +1,7 @@
 import SessionModel from '../models/session';
 
-export const getSession = (userId: string) => {
-  return SessionModel.findOne({ userId });
+export const getSession = async (userId: string) => {
+  const sessions = await SessionModel.find({ userId });
+  const currentSession = sessions.pop();
+  return currentSession;
 };
