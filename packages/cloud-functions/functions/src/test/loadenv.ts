cc0d808e7cbde03457522d8ad5f9f78ca9b0080c
Matt Cherry <mrcherry@uwaterloo.ca>
2019-08-26 12:10:17 -0400

    Load local env

--- packages/cloud-functions/functions/src/test/loadenv.ts
+++ packages/cloud-functions/functions/src/test/loadenv.ts
@@ -5,7 +5,7 @@ const envFile = '.env.staging.yaml';
 
 export const loadLocalEnv = () => {
   const file = fs.readFileSync(envFile, 'utf8');
-  const doc = YAML.parseDocument(file);
-  // @ts-ignore
-  process.env = { ...process.env, ...doc.contents };
+  const doc = YAML.parse(file);
+  process.env = { ...process.env, ...doc };
+  console.log('Loaded local env');
 };
