0cc38fb8b1e7341c0e0beeb0626a44f8aa1d9788
Quinn Turner <quinnturnertech@gmail.com>
2019-08-30 19:31:35 -0400

    fix: Converting string to ObjectId

--- packages/web-api/src/services/session.ts
+++ packages/web-api/src/services/session.ts
@@ -7,12 +7,11 @@ import {
 import { SessionDoc, SessionModel } from '@caravan/buddy-reading-mongo';
 
 export const getSessionFromUserId = async (
-  userId: string,
+  userId: mongoose.Types.ObjectId,
   client: OAuth2Client
 ) => {
-  const userObjectId = mongoose.Types.ObjectId(userId);
   const sessionQuery: Partial<SessionDoc> = {
-    userId: userObjectId,
+    userId,
     client,
   };
   const sessionSort: SameKeysAs<Partial<SessionDoc>> = {
