e5b3e71fad589b0b0a1585d228db2e2277ffbaf4
Yusuf <yhbk12@gmail.com>
2019-08-31 10:14:02 -0400

    Amazon links only appear if theyre the right one (linking to product page, no bitly)

--- packages/web/src/components/AmazonBuyButton.tsx
+++ packages/web/src/components/AmazonBuyButton.tsx
@@ -6,7 +6,6 @@ export interface AmazonBuyButtonProps {
 }
 
 const useStyles = makeStyles(theme => ({
-  chip: {},
   leftIcon: {
     color: 'white',
     marginRight: theme.spacing(1),
@@ -17,16 +16,10 @@ export default function AmazonBuyButton(props: AmazonBuyButtonProps) {
   const classes = useStyles();
 
   // Note: for now we're just linking to the Amazon Books page
-  // const { link } = props;
+  const { link } = props;
 
   return (
-    <Link
-      className={classes.chip}
-      href={
-        'https://www.amazon.com/b?_encoding=UTF8&tag=caravanclub0a-20&linkCode=ur2&linkId=7c2b4dc572c63afc5fd43f7a00dfbeaf&camp=1789&creative=9325&node=283155'
-      }
-      target={'_blank'}
-    >
+    <Link href={link} target={'_blank'}>
       <Typography variant="caption">BUY ON AMAZON</Typography>
     </Link>
   );
