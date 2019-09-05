12e19bf8903b9354e3c55b435d9a5addc2d5b4fc
Yusuf <yhbk12@gmail.com>
2019-09-05 02:22:50 -0400

    feat(feed): added random shelf invite messages

--- packages/web/src/common/getRandomInviteMessage.ts
+++ packages/web/src/common/getRandomInviteMessage.ts
@@ -4,9 +4,23 @@ export function getRandomInviteMessage(
   clubId: string
 ) {
   const messages = [
-    `Hey! ${userName} has invited you to join the club "${clubName}". You can check it out here: https://caravanapp.ca/clubs/${clubId}.`,
-    `Pssst... ${userName} has invited you to join the club "${clubName}" (it's super exclusive). Knock 3 times when you arrive: https://caravanapp.ca/clubs/${clubId}.`,
-    `Yoohoo! I've got a secret... ${userName} has invited you to join the club "${clubName}". Here's the link: https://caravanapp.ca/clubs/${clubId}. The password for the gate is "swordfish".`,
+    `Hey! ${userName} has invited you to join the club "${clubName}". You can check it out here: https://caravanapp.ca/clubs/${clubId}`,
+    `Pssst... ${userName} has invited you to join the club "${clubName}" (it's super exclusive). Knock 3 times when you arrive: https://caravanapp.ca/clubs/${clubId}`,
+    `Yoohoo! I've got a secret... ${userName} has invited you to join the club "${clubName}". Here's the link: https://caravanapp.ca/clubs/${clubId} The password for the gate is "swordfish".`,
+  ];
+  return messages[Math.floor(Math.random() * messages.length)];
+}
+
+export function getRandomInviteMessageFromShelf(
+  userName: string,
+  clubName: string,
+  clubId: string
+) {
+  const messages = [
+    `Hey! ${userName} created a club based on a shelf you liked! The club's called "${clubName}" and they'd love for you to join. You can check it out here: https://caravanapp.ca/clubs/${clubId}`,
+    `Pssst... ${userName} created a club based on a shelf you liked. The club's name is "${clubName}" and YOU are invited (it's super exclusive, so be proud). Knock 3 times when you arrive: https://caravanapp.ca/clubs/${clubId}`,
+    `Yoohoo! I've got a secret... ${userName} created the "${clubName}" club based on a shelf you liked. Best part? You're invited to join :) Here's the link: https://caravanapp.ca/clubs/${clubId}. The password for the gate is "swordfish".`,
+    `Here's the thing...you liked a shelf, then ${userName} created the club "${clubName}" based on the books on that shelf. Andddddd...yeah, let's just say you're invited :) NOT A BIG DEAL - STAY CALM! Here's the link: https://caravanapp.ca/clubs/${clubId}`,
   ];
   return messages[Math.floor(Math.random() * messages.length)];
 }
