cdce0f3570d838022fe66385a2d8220ee33a04af
Matt Cherry <mrcherry@uwaterloo.ca>
2019-09-18 18:03:24 -0400

    fix(API): jest/lerna/yarn workspaces config

--- packages/web-api/jest.config.js
+++ packages/web-api/jest.config.js
@@ -2,4 +2,17 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  cacheDirectory: '.jest-cache',
+  coverageDirectory: '.jest-coverage',
+  coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
+  coverageReporters: ['html', 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  testPathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
 };
