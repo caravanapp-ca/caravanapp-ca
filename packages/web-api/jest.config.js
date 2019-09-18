d77856e1cd5d6dde1e6cce79da361dee4f2e4337
Matt Cherry <mrcherry@uwaterloo.ca>
2019-09-18 18:16:38 -0400

    Revert "fix(API): jest/lerna/yarn workspaces config"
    This reverts commit cdce0f3570d838022fe66385a2d8220ee33a04af.

--- packages/web-api/jest.config.js
+++ packages/web-api/jest.config.js
@@ -2,17 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  cacheDirectory: '.jest-cache',
-  coverageDirectory: '.jest-coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
-  coverageReporters: ['html', 'text'],
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
-  testPathIgnorePatterns: ['<rootDir>/packages/(?:.+?)/lib/'],
 };
