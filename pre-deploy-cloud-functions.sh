faad3fb45a8768f062c35565b8e4c03d96be7f4e
Quinn Turner <quinnturnertech@gmail.com>
2020-03-06 09:38:41 -0500

    fix: Cloud function deployment path

--- pre-deploy-cloud-functions.sh
+++ pre-deploy-cloud-functions.sh
@@ -3,5 +3,5 @@ set -e
 yarn workspace @caravanapp/mongo lint
 yarn workspace @caravanapp/mongo build
 
-mkdir -p packages/cloud-functions/functions/src/workspace/mongo
-cp -r packages/mongo/dist/* packages/cloud-functions/functions/src/workspace/mongo
+mkdir -p cloud-functions/functions/src/workspace/mongo
+cp -r packages/mongo/dist/* cloud-functions/functions/src/workspace/mongo
