7a616518bc6d69aa81b4b2679ff332a493c4fa7e
Quinn Turner <quinnturnertech@gmail.com>
2020-03-09 20:22:01 -0400

    Fix unit testing

--- packages/web/src/App.test.tsx
+++ packages/web/src/App.test.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-it('can run a test', () => {
-  expect(2 + 2).toEqual(4);
-});
+describe('basic app tests', () => {
+  it('can run a test', () => {
+    expect(2 + 2).toEqual(4);
+  });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
