1366b644a3a3bb2d1f030151767b2af8b67030c5
Quinn Turner <quinnturnertech@gmail.com>
2020-03-30 14:58:35 -0400

    Only use dotenv in non-prod web-api

--- packages/web-api/src/index.ts
+++ packages/web-api/src/index.ts
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
